9a86be0bdec36d8d516ff17064cd4a77dfa05191
muddassarH <muddassarhussain90@gmail.com>
2023-05-07 19:28:49 +0500

    initial commit

--- Home.py
+++ Home.py
@@ -1,17 +1,15 @@
-import pandas
-import streamlit as st
 from fpdf import FPDF
 import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
-
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'])
     pdf.line(x1=10, y1=21, x2=200, y2=21)
-
+    for i in range(row['Pages']-1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
